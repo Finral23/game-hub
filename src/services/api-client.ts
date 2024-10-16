95c8eaaa45a695aff3865746dc351e90a39e98c1
Finral23 <grishanov23@gmail.com>
2024-10-16 18:25:31 +0300

    Build game attributes

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -27,7 +27,9 @@ class APIClient<T> {
   };
 
   get = (id: number | string) => {
-    return axiosInstance.get(this.endpoint + "/" + id).then((res) => res.data);
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((res) => res.data);
   };
 }
 
